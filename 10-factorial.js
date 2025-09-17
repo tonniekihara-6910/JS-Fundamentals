e554278f6c50706b34511c21633c0f1e62ca41b9
Anthony Waithaka <tonnie.kihara@gmail.com>
2025-09-17 18:13:27 +0300

    Add 10-factorial.js correct version

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,14 +1,10 @@
 #!/usr/bin/env node
 
-const arg = process.argv[2];
+const arg = parseInt(process.argv[2]);
 
 function factorial(n) {
   if (isNaN(n) || n <= 1) return 1;
   return n * factorial(n - 1);
 }
 
-if (!arg) {
-  console.log("No argument");
-} else {
-  console.log("Factorial " + factorial(parseInt(arg)));
-}
+console.log(factorial(arg));
