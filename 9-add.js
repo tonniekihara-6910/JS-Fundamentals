111096489d25a3c64c5d47182d23d566959790aa
Anthony Waithaka <tonnie.kihara@gmail.com>
2025-09-17 17:00:23 +0300

    Fix 9-add.js to pass checker

--- 9-add.js
+++ 9-add.js
@@ -5,11 +5,8 @@ function add(a, b) {
 const arg1 = process.argv[2];
 const arg2 = process.argv[3];
 
-if (!arg1 && !arg2) {
-  console.log('no argument');
-} else if (arg1 && !arg2) {
-  console.log('argument:', arg1);
+if (!arg1 || !arg2) {
+  console.log('NaN');
 } else {
-  console.log('arguments:', arg1, arg2);
   console.log(add(parseInt(arg1), parseInt(arg2)));
 }
